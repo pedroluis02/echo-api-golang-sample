31327163dc08d08fae5cc1ce9ea196f170b948e9
Pedro Luis <pedroluisnr22@gmail.com>
2023-07-06 12:04:47 -0500

    register controller and start server

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	controller "github.com/pedroluis02/echo-api-sample1/controller"
 )
 
 func main() {
 	fmt.Println("API with ECHO Framework")
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	controller.NewAuthService(e)
+	controller.NewUserService(e)
+
+	go func() {
+		if err := e.Start(":6000"); err != nil && err != http.ErrServerClosed {
+			fmt.Println(err)
+			fmt.Println("Shutting down the server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		fmt.Println(err.Error())
+	}
 }
